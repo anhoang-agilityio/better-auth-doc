bd83bfc841a21e6847d9bbc813788f227313ddf4
Hoang Doan An <an.hoang@asnet.com.vn>
2025-09-05 14:01:02 +0700

    feat: update Astro configuration for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,31 @@
+import process from 'node:process';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sanity from '@sanity/astro';
+
+import vercel from '@astrojs/vercel';
+
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.NODE_ENV,
+  process.cwd(),
+  '',
+);
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
     sanity({
-      projectId: '0gl88wwy',
-      dataset: 'production',
-      useCdn: false, // for static builds
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      apiVersion: '2025-08-18',
+      useCdn: true,
     }),
   ],
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
@@ -24,4 +34,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
